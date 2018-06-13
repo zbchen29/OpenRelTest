c0c635f6cf5048a190bfaae8b8832d4ae5511ba6
Zhenbang <zhenbang@mit.edu>
2018-06-13 16:21:08 -0400

    First commit

---
+++ Assets/Example/PrefabScripts/Receiver2Script.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using OpenRelativity.Objects;
+
+namespace OpenRelativity.PrefabScripts
+{
+    public class Receiver2Script : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void OnTriggerEnter(Collider collider)
+        {
+            //If an object crashes into us, set their death time so they know when to disappear.
+            collider.gameObject.GetComponent<RelativisticObject>().SetDeathTime();
+        }
+    }
+}
